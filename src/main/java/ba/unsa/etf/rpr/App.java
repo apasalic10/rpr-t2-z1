219ccc86fab9933e3dd3dd869a6e1a4f09402f17
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-10-24 15:16:36 +0200

    Tutorijal2

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,17 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws NeispravanParametarException {
+        try{
+            if(args[0].equals("faktorijel")){
+                System.out.println(Matematika.faktorijel(Integer.parseInt(args[1])));
+            }
+            else{
+                System.out.println(Matematika.sinus(Integer.parseInt(args[1])));
+            }
+        }catch (NeispravanParametarException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/ba/unsa/etf/rpr/NeispravanParametarException.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr;
+
+public class NeispravanParametarException extends Exception{
+    public NeispravanParametarException(String message) {
+        super(message);
+    }
+}
